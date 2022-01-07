7172a02e3cdcce6ec48d8e5da036a0889b90eb85
KVK <mrkvk2015@gmail.com>
2022-01-07 17:00:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, redirect, request
 from binance_average_buy import *
 
 app = Flask(__name__)
